9171d473a015e5e4c9bd374b2163ee770e01bf1b
Filip Edberger <filed881@student.liu.se>
2023-04-03 06:35:27 +0000

    Upload New File

---
+++ sensorGang/I2C.c
@@ -0,0 +1,188 @@
+/*
+ * IC2.c
+ *
+ * Created: 2023-03-31 10:15:58
+ * Author : filed881
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+#define I2C_BUFF_SIZE 2
+#define TW_MT_DATA_ACK     0x28
+
+volatile uint8_t I2C_read_buffer[I2C_BUFF_SIZE];
+
+static volatile int pulseCnt = 0; //Ska skickas, antal pulser från halleffect
+static volatile int i = 0;
+static volatile int pulse = 0; //Ska skickas pulsbredden från ultrasonic
+
+
+
+void i2c_init()
+{
+	TWAR = 0xD4; //Adress
+	
+	//TWCR = (1<<TWEN)|(1<<TWEA)|(1<<TWINT);
+	
+	TWCR = 0xC5;
+}
+
+void port_init()
+{
+	//ULTRALJUD
+	DDRD |= (1 << DDD5); //PD5 Output
+	DDRD |= (0 << DDD2); //PD2 (INT0) Input
+	
+	//HALLEFFECT
+	DDRD |= (0 << DDD3); //PD3 (INT1) Input
+	
+	
+	//TESTNING
+	DDRA = 0xFF; //Port A Output
+	
+}
+
+//TIMER TILL ULTRASONIC
+void timer0_init()
+{
+	TCCR0A = (1<<WGM01); //CTC mode
+	TCCR0B = (1<<CS02) | (1<<CS00); //Prescaler 1024
+	
+	OCR0A = 255; //Hur ofta vi vill generera avbrott på A
+	
+	OCR0B = 1; //Hur ofta vi vill generera avbrott på A
+	
+	TIMSK0 = (1<<OCIE0A) | (1<<OCIE0B); //Enable interrupt på A och B
+}
+
+//TIMER TILL HALLEFFECT
+void timer1_init()
+{
+	TCCR1B = (1<<CS12) | (1<<CS10) | (1<<WGM12);// Startar timer CTC prescaler 1024
+	
+	TCNT1 = 0;
+
+	OCR1A = 15625; //Hur ofta avbrott
+	
+	TIMSK1 = 1 << OCIE1A; //Aktiverar avbrott på A
+}
+
+//INTERRUPT TILL HALLEFFECT
+void int1_init()
+{
+	EIMSK |= 1 << INT1; //Enable INT1 interrupt
+	
+	EICRA |= (1<<ISC11) | (1<<ISC10); //Interrupt on rising edge
+}
+
+//INTERRUPT TILL ULTRASONIC
+void int0_init()
+{
+	EIMSK |= 1 << INT0; //Enable av INT0
+	
+	EICRA |= (1 << ISC00); //Any edge interrupt
+}
+
+int main(void)
+{
+	port_init();
+	
+	int1_init();
+	
+	int0_init();
+	
+	timer1_init();
+	
+	timer0_init();
+	
+	i2c_init();
+	
+	sei();
+	
+	while (1)
+	{
+		printf(1);
+	}
+}
+
+//INTERRUPTRUTIN HALLEFFECT
+ISR(TIMER1_COMPA_vect)
+{
+	//PORTA = pulseCnt;
+	
+	I2C_read_buffer[1] = pulseCnt;
+	
+	pulseCnt = 0;
+	
+	//TCNT1=0;
+}
+
+//INTERRUPTRUTIN ULTRASONIC
+ISR(TIMER0_COMPA_vect)
+{
+	PORTD = 1<<PORTD5;
+}
+
+//INTERRUPTRUTIN ULTRASONIC
+ISR(TIMER0_COMPB_vect)
+{
+	PORTD = 0<<PORTD5;
+}
+
+//INTERRUPTRUTIN INT1 HALLEFFECT
+ISR(INT1_vect)
+{
+	pulseCnt++;
+}
+
+//INTERRUPTRUTIN INT0 ULTRASONIC
+ISR(INT0_vect)
+{
+	if(i) //Disable counter
+	{
+		TCCR3B = 0;  //Stänger av timer
+		
+		pulse = TCNT3; //Sparar värdet från timern
+		
+		I2C_read_buffer[0] = TCNT3;
+		
+		PORTA = TCNT3;
+		
+		TCNT3 = 0; //Nollställer timer
+		
+		i = 0;
+	}
+	
+	else
+	{
+		TCCR3B = (1 << CS32)|(1 << CS30); //Prescaler 1028
+		
+		i = 1;
+	}
+}
+
+ISR(TWI_vect)
+{
+	switch((TWSR & 0xF8)) {
+		
+		case 0x60: //SLA+W has been recieved, ACK has been returned
+			break;
+		 
+	}
+	
+	/*for (i = 0; i < 2; i++)
+	{
+		TWDR = I2C_read_buffer[i];
+		TWCR = (1<<TWEN) | (1<<TWINT);
+		while (!(TWCR & (1<<TWINT)));
+		if ((TWSR & 0xF8) != TW_MT_DATA_ACK) return -1;
+	}*/
+
+	//TWCR = (1<<TWEN) | (1<<TWINT) | (1<<TWSTO);
+	
+	//TWDR = pulse;
+	//TWCR = 0xC5;
+}
+
