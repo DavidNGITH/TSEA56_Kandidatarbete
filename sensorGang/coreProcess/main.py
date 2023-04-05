9ed9eeee46b6e48a39135040963b3174d1e506ca
vicer124 <vicer124@student.liu.se>
2023-04-05 11:12:22 +0200

    Initial work on core process of the project

---
+++ sensorGang/coreProcess/main.py
@@ -0,0 +1,17 @@
+import multiprocessing as mp
+
+#Initializing functions
+
+
+#Main-loop
+while(True):
+
+    #Retrieve inputs from Computer
+
+    #Collect data from camera and calculate center off-set
+
+    #Retrieve data from AVR
+
+    #Send data to AVR
+
+
