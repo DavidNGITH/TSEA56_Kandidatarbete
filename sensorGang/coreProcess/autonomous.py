492a3dddeb3aca73e699b2ba9ee5dc2a491d11c8
Filip Edberger <filed881@student.liu.se>
2023-04-11 20:43:15 +0200

    Minor changes

--- sensorGang/coreProcess/autonomous.py
+++ sensorGang/coreProcess/autonomous.py
@@ -14,41 +14,49 @@ class Autonomous():
         self.status = True
     
         self.mqttClient = mqttClient
-        self.mqttClient.on_message = self.on_message
+        self.mqttClient.on_message = self.onMessage
         self.mqttClient.subscribe(MQTT_TOPIC)
 
         self.qMessage = multiprocessing.Queue()
         self.qCenterOffset = multiprocessing.Queue()
 
-        self.p1 = multiprocessing.Process(target=self.handel_message, args=(self.qMessage,))
+        self.p1 = multiprocessing.Process(target=self.handleMessage, args=(self.qMessage,))
         self.p2 = multiprocessing.Process(target=self.laneData.getCenterOffset, args =(self.qCenterOffset,))
 
-    def on_message(self,client, userdata, message):
+        self.p1.start()
+        self.p2.start()
+
+    def onMessage(self,client, userdata, message):
         try:
             m = int(message.payload.decode("utf-8"))
-            t = message.topic.decode("utf-8")
+            t = message.topic
             self.qMessage.put((t,m))
         except:
             print("Couldn't read mqtt message")
 
-    def handel_message (self,q):
+    def handleMessage (self,q):
+        print("In handleMessage autonomous!")
         pingTime = time.time()
         while True:
             if not q.empty():
+                print("qMessage not empty")
                 if q.get()[0] == "stop":
+                    print("Recived stop in autonomous")
                     self.stop()
                     return
                 elif q.get()[0] == "ping":
+                    print("Recived ping in autonomous")
                     pingTime = time.time()
 
-            if time.time() - pingTime > 2:
+            if time.time() - pingTime > 30:
                 self.stop()
                 return
+            time.sleep(0.01)
 
     def stop(self):
         #Stop all motors
         self.laneData.stopProcess()
-        self.p2.kill()
+        self.p2.join()
         self.mqttClient.disconnect()
         self.status = False
        
@@ -58,4 +66,5 @@ class Autonomous():
             if not self.qCenterOffset.empty():
                 centerOffset = self.qCenterOffset.get()
                 #Send data to PD-controller
+            time.sleep(0.01)
         
