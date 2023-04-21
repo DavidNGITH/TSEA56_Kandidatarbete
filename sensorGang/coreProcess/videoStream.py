c21c87a2c6a51fcebf99e5e4edbffb79860b3d37
Filip Edberger <filed881@student.liu.se>
2023-04-21 17:05:38 +0200

    Changed frame rate

--- sensorGang/coreProcess/videoStream.py
+++ sensorGang/coreProcess/videoStream.py
@@ -9,7 +9,7 @@ import time
 class VideoStream:
     """Class for video stream."""
 
-    def __init__(self, resolution, framerate=60):
+    def __init__(self, resolution, framerate=20):
         """Init camera."""
         self.camera = PiCamera()
         self.camera.resolution = resolution
