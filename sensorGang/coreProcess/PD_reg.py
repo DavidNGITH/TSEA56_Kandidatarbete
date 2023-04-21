0a3a85fa245918f25bf900ca7f5e495b1ee7c472
Eric Davidsson <erida646@student.liu.se>
2023-04-21 15:58:21 +0000

    vänstersväng test

--- sensorGang/coreProcess/PD_reg.py
+++ sensorGang/coreProcess/PD_reg.py
@@ -1,43 +1,42 @@
-"""PD Controller."""
 import time
 
 
 class PDcontroller:
-    """Class for PD controller."""
 
     def __init__(self, Kp, Kd):
-        """Init variables."""
         self.Kp = Kp
         self.Kd = Kd
         self.derivative_term = 0
         self.last_error = None
-        self.last_time = 0
+        self.last_time=0
         self.steering = 0
 
     def get_control(self, center_offset):
-        """Calculate steering."""
         error = center_offset
         if self.last_error is not None:
-            self.derivative_term = (error-self.last_error) / \
-                (time.time()-self.last_time)*self.Kd
-            self.last_time = time.time()
-        self.PD_value = self.Kp*error + self.derivative_term
+            self.derivative_term = (error-self.last_error)/(time.time()-self.last_time)*self.Kd
+            self.last_time=time.time()
+        self.PD_value= self.Kp*error + self.derivative_term
         self.last_error = error
         self.update_steering()
         return self.steering
 
+
     def update_steering(self):
-        """Check threshold."""
-        #print(self.PD_value)
         if abs(self.PD_value) > 10:
-            self.steering = self.PD_value
+            print(self.PD_value)
+            if self.PD_value < 0:
+                self.steering = (self.PD_value *0.8)
+            elif self.PD_value > 0:
+
+                self.steering = self.PD_value
         else:
             self.steering = 0
 
+
     def updateKp(self, Kp):
-        """Update Kp value."""
         self.Kp = Kp
 
     def updateKd(self, Kd):
-        """Update Kd value."""
-        self.Kp = Kd/100
+        self.Kp = Kd
+
