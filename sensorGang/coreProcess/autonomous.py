8342a351decd435b14b13707b9381f04a13584b3
Filip Edberger <filed881@student.liu.se>
2023-04-26 15:20:26 +0200

    nmore distance

--- sensorGang/coreProcess/autonomous.py
+++ sensorGang/coreProcess/autonomous.py
@@ -121,11 +121,11 @@ class Autonomous():
             if time.time() - i2cTimeElapsed > 0.1:
                 try:
                     data = I2C_proc.get()
-                    if (int(data[0][1]) < 40) & (self.object is False):
+                    if (int(data[0][1]) < 60) & (self.object is False):
                         print("Obstacle")
                         I2C_proc.send((2, 1))
                         self.object = True
-                    elif (int(data[0][1]) >= 40) & (self.object):
+                    elif (int(data[0][1]) >= 60) & (self.object):
                         print("Release")
                         I2C_proc.send((2, 0))
                         self.object = False
