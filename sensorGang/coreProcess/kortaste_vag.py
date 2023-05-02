c74dd9d5c3771f338041b2455482e8a844fe2293
Victor Eriksson <vicer124@student.liu.se>
2023-05-02 19:19:20 +0200

    TEST

--- sensorGang/coreProcess/kortaste_vag.py
+++ sensorGang/coreProcess/kortaste_vag.py
@@ -12,7 +12,7 @@ def read_graph_from_file(road_map):
                 graph[node1] = {}
             if node2 not in graph:
                 graph[node2] = {}
-            graph[node1][node2] = weight
+            graph[node1][node2] = [weight, int(direction)]
             # graph[node2][node1] = weight
     print(graph)
     return graph
@@ -25,7 +25,10 @@ def kortaste_vag(graph, start, goal):
 
     path = {}
     path[start] = [start]
+    directions = dict()
+    # directions[start] = [start]
 
+    directionlist = list()
     unvisited = set(graph)  # sätter alla noder till obesökta
 
     while unvisited:
@@ -39,14 +42,21 @@ def kortaste_vag(graph, start, goal):
         unvisited.remove(current_node)  # tar bort besökt nod
 
         if current_node == goal:
-            return path[current_node]
+            for i in directions:
+                if i in path[current_node]:
+                    print(directions[i])
+                    directionlist.append(directions[i])
+            return path[current_node], directionlist
 
         # Uppdaterar distans och kortaste väg för varje granne till nuvarande nod
-        for neighbor, weight in graph[current_node].items():
-            new_distance = distance[current_node] + weight
+        for neighbor, weightdist in graph[current_node].items():
+            new_distance = distance[current_node] + weightdist[0]
             if new_distance < distance[neighbor]:
                 distance[neighbor] = new_distance
                 path[neighbor] = path[current_node] + [neighbor]
+
+                directions[neighbor] = weightdist[1]
+                # print(path)
 
     # Returnera None om det inte finns väg från start till slut
     return None
